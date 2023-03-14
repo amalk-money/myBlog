e157baf5644aa06f3ca2129eaa1e3fddcad0bc6f
amalk-money <amalkmoney1998@gmail.com>
2023-03-14 20:47:47 +0530

    Blog urls has been updated.

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
-def index(index):
+def home(index):
+    return HttpResponse("<h1>Hi There! This is blog home page</h1>")
+def blog(index):
     return HttpResponse("<h1>Hi There! This is blog page</h1>")
