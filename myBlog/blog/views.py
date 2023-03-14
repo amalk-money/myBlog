2218d57ab629543936a3aa6efba21c7bd7350e22
amalk-money <amalkmoney1998@gmail.com>
2023-03-14 20:17:40 +0530

    Blog urls has been included to myBlog urls.

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def index(index):
+    return HttpResponse("<h1>Hi There! This is blog page</h1>")
