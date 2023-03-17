50f3d693941a7aeb33d38e7e11216a1895a8b01c
amalk-money <amalkmoney1998@gmail.com>
2023-03-17 23:02:56 +0530

    "Contact" - model created. contact page added appconfig added to settings.py file model registered in admin.py file

--- myBlog/home/models.py
+++ myBlog/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    userID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    email = models.CharField(max_length=100)
+    query = models.CharField(max_length=500)
+    date = models.DateTimeField(auto_now_add = True)
