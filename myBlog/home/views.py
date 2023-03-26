1a103a0bb3eb487d5cd57b3232759d56a9804b83
amalk-money <amalkmoney1998@gmail.com>
2023-03-26 19:07:21 +0530

    made changes to search page

--- myBlog/home/views.py
+++ myBlog/home/views.py
@@ -28,6 +28,13 @@ def about(request):
 
 def search(request):
     query = request.GET['search']
-    allPosts = Post.objects.filter(content__icontains=query)
+    if len(query) > 80:
+        allPosts = Post.objects.none()
+    else:
+        allPostsTitle = Post.objects.filter(title__icontains=query)
+        allPostsContent = Post.objects.filter(content__icontains=query)
+        allPostsAuthor = Post.objects.filter(author__icontains=query)
+        allPosts = allPostsTitle.union(allPostsContent, allPostsAuthor)
+
     params = {'allPosts': allPosts, 'query':query}
     return render(request, 'home/search.html',params)
