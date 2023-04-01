079111a54eafe801bbfb7c2868ce5395efdf1593
amalk-money <amalkmoney1998@gmail.com>
2023-04-02 00:28:15 +0530

    comments added successfully

--- myBlog/blog/urls.py
+++ myBlog/blog/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #API to use post comments
+    path('postComment', views.postComment, name="postComment"),
+
     path('', views.blogHome, name="blogHome"),
     path('<str:slug>/', views.blogPost, name="blogPost"),
+
+
 ]
--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
-from blog.models import Post
+from django.shortcuts import render, HttpResponse, redirect
+from blog.models import Post, Comments
 def blogHome(request):
     allPosts = Post.objects.all()
     params = {'allPosts':allPosts}
@@ -7,5 +7,20 @@ def blogHome(request):
 
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug).first()
-    param = {'post':post}
+    comments = Comments.objects.filter(post=post)
+    param = {'post':post, 'comments':comments}
     return render(request, 'blog/blogPost.html', param)
+
+from django.contrib import messages
+def postComment(request):
+    if request.method == "POST":
+        comment = request.POST.get('comment')
+        user = request.user
+        postSNo = request.POST.get('postSNo')
+        post = Post.objects.get(postID=postSNo)
+
+        comment = Comments(comment=comment, user=user, post=post)
+        comment.save()
+        messages.success(request, "Your comments posted.")
+
+    return redirect(f"/blog/{post.slug}/")
